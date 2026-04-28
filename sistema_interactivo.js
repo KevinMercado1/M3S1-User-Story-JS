6cd67fac0afe2de823675ea99a0162291192a3ea
Kevin Mercado <141512106+KevinMercado1@users.noreply.github.com>
2026-04-28 16:25:28 -0500

    finished

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,10 +1,19 @@
-const name = prompt("What's your name?:", "")
-const age = Number(prompt("How old are you?:", ""))
+// En este apartado está las constantes definidas//
+const userName = prompt('Cúal es tú nombre?:', '').trim();
+const age = Number(prompt('Cuantos años tienes?:', '').trim());
 
+// Aquí va los condicionales que hacen preguntar la edad del cliente.//
 if (isNaN(age)) {
-    console.error('Please, the age must be a number!');
+  console.error('Por favor, deben ser NUMEROS!');
+  alert('Error: Debes ingresar un número para la edad.');
 } else if (age >= 18) {
-    alert(`Hola ${name}, tienes ${age} años y eres mayor de edad. ¡Prepárate para grandes oportunidades!`);
+  //Aquí esta el condicional que hace sí es MAYOR de edad.//
+  alert(
+    `Hola ${userName}, tienes ${age} años y eres mayor de edad. ¡Prepárate para grandes oportunidades!`
+  );
 } else {
-    alert(`Hola ${name}, tienes ${age} años y eres menor de edad. ¡Sigue aprendiendo!`);
+  //Aquí está el condicional que se ejecuta si es MENOR de edad.//
+  alert(
+    `Hola ${userName}, tienes ${age} años y eres menor de edad. ¡Sigue aprendiendo!`
+  );
 }
